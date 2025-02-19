d9785c9753663bdcce4fc1c597628f8f924284ba
Max-Lovell <max_lovell@hotmail.co.uk>
2025-02-19 16:24:25 +0000

    Comments clarifying trigger controller

--- Psychopy/Triggers.py
+++ Psychopy/Triggers.py
@@ -8,13 +8,14 @@ def trigger(*lines, close=False):
     global bitmask
     if 0 in lines:
         bitmask = 0
-       
+    
+    # bitmask is 00000000 when all lines are closed, and e.g. 01000000 when line 2 is open, 01000001 for lines 2 and 8 open
     for line in lines:
         if line != 0:
             if close:
-               bitmask &= (0 << (line-1))
+               bitmask &= (0 << (line-1)) # set to 0 and AND with bitmask to close line
             else:
-                bitmask |= (1 << (line-1)) #add 1 in index of line, and OR with bitmask
+                bitmask |= (1 << (line-1)) #add 1 in index of line, and OR with bitmask to open line
     #convert to hex
     hexValue = format(bitmask, '02x')
     port.write(hexValue.encode()) # Change port to whatever your port is
